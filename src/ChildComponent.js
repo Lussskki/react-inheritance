ff1fa27530935f70414ef0844b96bf70b17cace1
Luka Guledani <lukaguledani123@gmail.com>
2024-07-24 18:05:19 +0400

    first commit

---
+++ src/ChildComponent.js
@@ -0,0 +1,13 @@
+import react from "react"
+
+const ChildComponent = (props) => {
+    return (
+
+        <div>
+            <p>Message from: {props.message} </p>
+        </div>
+
+    )
+}
+
+export default ChildComponent
